ca0d2ebab5631d0b9f6eecdc53841b8bf14160dd
Fabian Loosli <fabian.loosli@students.unibe.ch>
2017-10-07 13:09:46 +0200

    finished Beginner Guide

---
+++ workspace-sts-3.9.0.RELEASE/ESE4_Database/src/main/java/Database/UserRepository.java
@@ -0,0 +1,12 @@
+package Database;
+
+import org.springframework.data.repository.CrudRepository;
+
+import Database.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
