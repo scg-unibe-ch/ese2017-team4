70bef9efa26aa5417bf82f461f12de1b948a9cbc
Simon Sidler <simon.sidler@students.unibe.ch>
2017-11-21 19:51:35 +0100

    input validation for addPackage

--- workspace-sts-3.9.0.RELEASE/ese2017-team4-project/src/main/java/ese4/controller/PackageController.java
+++ workspace-sts-3.9.0.RELEASE/ese2017-team4-project/src/main/java/ese4/controller/PackageController.java
@@ -3,9 +3,12 @@ package ese4.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -36,7 +39,12 @@ public class PackageController {
     }
 
     @PostMapping("/addPackageForm")
-    public String formSubmit(@ModelAttribute Package pack) {
+    public String formSubmit(@Valid Package pack, BindingResult bindingResult) {
+    	if (bindingResult.hasErrors()) {
+    		System.out.println("test");
+    		return "package/addPackageForm";
+    	}
+    	
     	packageRepository.save(pack);
         return "homescreen";
     }
