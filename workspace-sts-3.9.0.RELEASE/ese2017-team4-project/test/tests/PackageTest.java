abfd94869b2edc393a6c01fdf8967cee055ab035
Loosli <fabian.loosli@students.unibe.ch>
2017-12-07 09:08:09 +0100

    A Pile of tests

--- workspace-sts-3.9.0.RELEASE/ese2017-team4-project/test/tests/PackageTest.java
+++ workspace-sts-3.9.0.RELEASE/ese2017-team4-project/test/tests/PackageTest.java
@@ -13,25 +13,56 @@ public class PackageTest {
 	public void constructor(){
 		testPackage = new Package("Bern",5, 6, 7, 8);
 	}
-
+	
+	@Test
+	public void getPackageTest() {
+		assertEquals("Bern", testPackage.getAddress());
+		assertTrue((5.1 > testPackage.getWeight()) && (4.9 < testPackage.getWeight()));
+		assertTrue((6.1 > testPackage.getHeight()) && (5.9 < testPackage.getHeight()));
+		assertTrue((7.1 > testPackage.getLength()) && (6.9 < testPackage.getLength()));
+		assertTrue((8.1 > testPackage.getWidth()) && (7.9 < testPackage.getWidth()));
+	}
+	
+	@Test
+	public void setPackageTest() {
+		testPackage.setAddress("Zürich");
+		testPackage.setWeight(9);
+		testPackage.setHeight(8);
+		testPackage.setLength(5);
+		testPackage.setWidth(6);
+		assertEquals("Zürich", testPackage.getAddress());
+		assertTrue((9.1 > testPackage.getWeight()) && (8.9 < testPackage.getWeight()));
+		assertTrue((8.1 > testPackage.getHeight()) && (7.9 < testPackage.getHeight()));
+		assertTrue((5.1 > testPackage.getLength()) && (4.9 < testPackage.getLength()));
+		assertTrue((6.1 > testPackage.getWidth()) && (5.9 < testPackage.getWidth()));
+	}
+	
+	@Test 
+	public void setIDStatusTest() {
+		testPackage.setId(7);
+		testPackage.setStatus(Status.VERNICHTET);
+		assertTrue(7 == testPackage.getId());
+		assertTrue(Status.VERNICHTET == testPackage.getStatus());
+	}
+	
 	@Test
 	public void pendantPackageTest() {	
-		assertEquals(Status.PENDENT, testPackage.getIsDelivered());
-		assertEquals("pendent", testPackage.getIsStatus());
+		assertEquals(Status.PENDENT, testPackage.getStatus());
+		assertEquals("pendent", testPackage.getStatusDisplay());
 	}
 	
 	@Test
 	public void isPlacedInTourTest() {
 		testPackage.placedInTour();
-		assertEquals(Status.GEPLANT, testPackage.getIsDelivered());
-		assertEquals("geplant", testPackage.getIsStatus());
+		assertEquals(Status.GEPLANT, testPackage.getStatus());
+		assertEquals("geplant", testPackage.getStatusDisplay());
 	}
 	
 	@Test
 	public void setToDeliveredTest() {
 		testPackage.setToDelivered();
-		assertEquals( Status.ZUGESTELLT, testPackage.getIsDelivered());
-		assertEquals("zugestellt", testPackage.getIsStatus());
+		assertEquals( Status.ZUGESTELLT, testPackage.getStatus());
+		assertEquals("zugestellt", testPackage.getStatusDisplay());
 	}
 	
 	@Test
--- workspace-sts-3.9.0.RELEASE/ese2017-team4-project/test/tests/TourTest.java
+++ workspace-sts-3.9.0.RELEASE/ese2017-team4-project/test/tests/TourTest.java
@@ -15,31 +15,37 @@ public class TourTest {
 	private Tour testTour;
 	private Package testPackage1;
 	private Package testPackage2;
-	private User testUser;
+	private User testUser = new User();
 	
 	@Before
-	public void constructor()
-	{
+	public void constructor() {
 		testPackage1 = new Package("Bern", 4, 4, 4, 4);
 		testPackage2 = new Package("Zürich", 5, 5, 5, 5);
 		testTour = new Tour(packages, testUser);
 	}
 	
 	@Test
-	public void isFinishedTest()
-	{
+	public void getSetTourTest() {
+		testTour.setId(10);
+		testTour.setEstimatedDeliveryTime(60);
+		assertEquals(testUser, testTour.getDriver());
+		assertEquals(10, testTour.getId());
+		assertEquals(60, testTour.getEstimatedDeliveryTime());
+		
+	}
+	
+	@Test
+	public void isFinishedTest() {
 		assertFalse(testTour.getIsFinished());
 		testTour.setFinished();
 		assertTrue(testTour.getIsFinished());
 	}
 	
 	@Test
-	public void addPackagesToTourTest()
-	{
+	public void addPackagesToTourTest() {
 		testTour.addPackageToTour(testPackage1);
 		assertEquals(testPackage1, testTour.getPacks().get(0));
 		testTour.addPackageToTour(testPackage2);
 		assertEquals(testPackage2, testTour.getPacks().get(1));
 	}
-
 }
