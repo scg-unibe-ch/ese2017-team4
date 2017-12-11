f5499ba8a87056151518b523a9b8b676c436803c
Robin Steiner <robin95@bluewin.ch>
2017-12-11 14:56:22 +0100

    Commenting

--- workspace-sts-3.9.0.RELEASE/ese2017-team4-project/src/main/java/ese4/controller/TourController.java
+++ workspace-sts-3.9.0.RELEASE/ese2017-team4-project/src/main/java/ese4/controller/TourController.java
@@ -44,21 +44,33 @@ public class TourController {
 	@Autowired
 	private UserService userService;
 	
-	// Returns the user to the homescreen
+	/**
+	 * Returns the user to the homescreen
+	 * @return the homescreen html
+	 */
 	@GetMapping("/home")
     public String get() {
     	return "homeScreen";
     }
 	
-	// Sends the user to the /makeTour site
+	/**
+	 * Sends the user to the /makeTour site
+	 * @return the createTour html
+	 */
 	@GetMapping("/makeTour")
 	public String makeTour() {
 		return "tour/createTour";
 	}
 	
-	// The PostMapping for /createTour which creates the tour object
-	// adds all the packages to the Tour Object and sets the driver
-	// and estimated Time
+	/**
+	 * The PostMapping for /createTour which creates the tour object
+	 * adds all the packages to the Tour Object and sets the driver
+	 * and estimated Time
+	 * @param driverId Which Driver is concerned
+	 * @param packageIds the List of Packages concerned
+	 * @param estimatedDeliveryTime The estimated time the tour will take for the driver
+	 * @return the homescreen html
+	 */
 	@PostMapping("/createTour")
 	public String createTour(@RequestParam("driverId") Integer driverId,
 			@RequestParam("packageId") List<Integer> packageIds,
@@ -83,13 +95,21 @@ public class TourController {
 		return "homescreen";
 	}
 	
-	// Sends the user to /listAll
+	/**
+	 * Sends the user to /listAll
+	 * @return the listAllTours html
+	 */
     @GetMapping("/listAll")
     public String allTours() {    
     	return "tour/listAllTours";
     }
     
-    // Displays the selected tour
+    /**
+     * Displays the selected tour
+     * @param tourId Id of the Tour
+     * @param model
+     * @return the selectedTour html
+     */
     @PostMapping("/listSelectedTour")
     public String listSelectedTour(@RequestParam("tourId") Integer tourId, Model model) {
     	Tour tour = tourRepository.findById(tourId);
@@ -97,27 +117,41 @@ public class TourController {
     	return "tour/selectedTour";
     }
     
-    // Gets the current user
+    /**
+     * Gets the current user
+     * @return user
+     */
     User getCurrentUser() {
     	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		User user = userService.findUserByName(auth.getName());
     	return user;
     }
     
-    // Sends the user to /listMyTour
+    /**
+     * Sends the user to /listMyTour
+     * @return the listMyTour html
+     */
     @GetMapping("/listMyTour")
     public String listMyTour() {
     	return "tour/listMyTour";
     }
     
-    // Sends the user to /confirmMyTour
+    /**
+     * Sends the user to /confirmMyTour
+     * @return the confirmMyTour html
+     */
     @GetMapping("/confirmMyTour")
     public String listMyTourToConfirm() {
     	return "tour/confirmMyTour";
     }
     
-    // Sets the statuses that the driver set on the /confirmMyTourPage
-    // And finishes the Tour
+    /**
+     * Sets the statuses that the driver set on the /confirmMyTourPage
+     * And finishes the Tour
+     * @param notDeliverablePackages
+     * @param deliveredPackages
+     * @return the homescreen html
+     */
     @PostMapping("/confirmMyTour")
     public String confirmMyTour(@RequestParam(value = "notDeliverablePackage", required=false) List<Integer> notDeliverablePackages, 
     		@RequestParam(value = "deliveredPackage", required=false) List<Integer> deliveredPackages) {
@@ -160,13 +194,19 @@ public class TourController {
     	return "homeScreen";
     }
       
-    // Handles the request to the database getting the notDelivered Packages
+    /**
+     * Handles the request to the database getting the notDelivered Packages
+     * @return pending or sent back packages
+     */
     @ModelAttribute("packagesNotDelivered")
     public Iterable<Package> allPackagesAsList() {
     		return this.packageRepository.findByStatusDisplayOrStatusDisplayOrderByDeliveryCounterDesc("pendent", "zurück gesendet");
     }
     
-    // Handles the requests to the database for the users
+    /**
+     * Handles the requests to the database for the users
+     * @return a List of drivers without a tour
+     */
     @ModelAttribute("drivers")
     public Iterable<User> allDriversAsList() {
     	List<User> allDrivers = (List<User>) this.userRepository.findbyRole("Driver");
@@ -186,19 +226,28 @@ public class TourController {
     	return driversWithoutTour;
     }
     
-    // Handles the database request for getting the tours
+    /**
+     * Handles the database request for getting the tours
+     * @return a List of all Tours
+     */
     @ModelAttribute("tours")
     public Iterable<Tour> allTourAsList() {
     	return this.tourRepository.findAll();
     }
     
-    // Handles the database request for finding a drivers tour
+    /**
+     * Handles the database request for finding a drivers tour
+     * @return a specific drivers tour
+     */
     @ModelAttribute("myTour")
     public Tour myTour() {
     	return this.tourRepository.findTourByDriverNotFinished(getCurrentUser());
     }
     
-    // Handles the database request for not confirmed tours
+    /**
+     * Handles the database request for not confirmed tours
+     * @return a list of unfinished tours
+     */
     @ModelAttribute("toursNotConfirmed")
     public Iterable<Tour> allToursAsList(){
     	return this.tourRepository.findByIsFinished(false);
--- workspace-sts-3.9.0.RELEASE/ese2017-team4-project/src/main/java/ese4/controller/UserController.java
+++ workspace-sts-3.9.0.RELEASE/ese2017-team4-project/src/main/java/ese4/controller/UserController.java
@@ -29,19 +29,28 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 
-	// Returns the user to the home page
+	/**
+	 * Returns the user to the home page
+	 * @return the homescreen html
+	 */
 	@GetMapping("/home")
 	public String get() {
 	    return "homeScreen";
 	}	
 	 
-	// Lists all the users under /listAll
+	/**
+	 * Lists all the users under /listAll
+	 * @return the listAllUsers html
+	 */
     @GetMapping("/listAll")
     public String listAllUsers() {        
     	return "user/listAllUsers";
     }	
     
-    // Handles the database request to list all users
+    /**
+     * Handles the database request to list all users
+     * @return a list of all users
+     */
     @ModelAttribute("users")
     public Iterable<User> allUserssAsList() {
     	return this.userRepository.findAll();
